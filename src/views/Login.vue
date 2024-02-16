04abd8b8d7a6fc2d5b7181948bdac23cf1e4dbab
[firstname <j6cafe2018@gmail.com>
2024-02-17 01:42:36 +0800

    zzz

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -24,12 +24,12 @@
                             <button
                                 type="button"
                                 class="btn btn-danger w-100"
-                                @click="socialiteLogin('google')" 
+                                @click="socialiteLogin('google')"
                             >
-                            <i class="fa fa-google"></i> Google
+                                <i class="fa fa-google"></i> Google
                             </button>
                         </div>
-                    
+
                         <form @submit.prevent="authenticate">
                             <!-- Email input -->
                             <div class="form-outline mb-4">
@@ -81,7 +81,7 @@
     </div>
 </template>
 <script>
-import { userStore } from '../stores/userStore';
+import { userStore } from "../stores/userStore";
 
 export default {
     data() {
@@ -99,10 +99,86 @@ export default {
     computed: {},
 
     methods: {
+        // socialiteLogin(provider) {
+        //     window.location.href = `${import.meta.env.VITE_API_URL}/auth/${provider}/redirect`;
+        // },
+
         socialiteLogin(provider) {
-            window.location.href = `${import.meta.env.VITE_API_URL}/auth/${provider}/redirect`;
+            const newWindow = this.openWindow("", "message");
+            axios({
+                method: "get",
+                url: `${
+                    import.meta.env.VITE_API_URL
+                }/auth/${provider}/redirect`,
+            })
+                .then((res) => {
+                    console.log(res.data);
+                    // newWindow.location.href = response.data;
+                })
+                .catch((err) => {
+                    console.log(err.response);
+                });
         },
-        
+
+        onMessage(e) {
+            if (e.origin !== window.origin || !e.data.token) {
+                return;
+            }
+            console.log(e.data);
+            userStore().$patch((state) => {
+                state.user = Object.assign({}, e.data.user, {
+                    access_token: res.data.access_token,
+                });
+                state.access_token = e.data.access_token;
+            });
+
+            this.$router.push("/dashboard");
+        },
+
+        openWindow(url, title, options = {}) {
+            if (typeof url === "object") {
+                options = url;
+                url = "";
+            }
+
+            options = { url, title, width: 600, height: 720, ...options };
+
+            const dualScreenLeft =
+                window.screenLeft !== undefined
+                    ? window.screenLeft
+                    : window.screen.left;
+            const dualScreenTop =
+                window.screenTop !== undefined
+                    ? window.screenTop
+                    : window.screen.top;
+            const width =
+                window.innerWidth ||
+                document.documentElement.clientWidth ||
+                window.screen.width;
+            const height =
+                window.innerHeight ||
+                document.documentElement.clientHeight ||
+                window.screen.height;
+
+            options.left = width / 2 - options.width / 2 + dualScreenLeft;
+            options.top = height / 2 - options.height / 2 + dualScreenTop;
+
+            const optionsStr = Object.keys(options)
+                .reduce((acc, key) => {
+                    acc.push(`${key}=${options[key]}`);
+                    return acc;
+                }, [])
+                .join(",");
+
+            const newWindow = window.open(url, title, optionsStr);
+
+            if (window.focus) {
+                newWindow.focus();
+            }
+
+            return newWindow;
+        },
+
         authenticate() {
             this.$refs.login_btn.setAttribute("disabled", true);
             axios({
@@ -131,7 +207,24 @@ export default {
 
     updated() {},
 
-    mounted() {},
+    beforeUnmount() {
+        window.removeEventListener("message", this.onMessage);
+    },
+
+    mounted() {
+        // Waiting for the callback.blade.php message... (token and username).
+        window.addEventListener("message", this.onMessage, false);
+        
+        // test
+        // axios({
+        //     method: 'get',
+        //     url: `${import.meta.env.VITE_API_URL}/test`,
+        // }).then(res => {
+        
+        // }).catch(err => {
+        
+        // });
+    },
 };
 </script>
 
