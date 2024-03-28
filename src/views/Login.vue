72a2f630c39c1d11bc9c3f8ed89a016144eb0ba3
[firstname <j6cafe2018@gmail.com>
2024-03-28 19:58:30 +0800

    broad

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -100,78 +100,80 @@ export default {
 
     methods: {
         socialiteLogin(provider) {
-            const newWindow = this.openWindow('', 'message');
+            // const newWindow = this.openWindow('', 'message');
 
             axios({
                 method: "GET",
                 url: `/api/auth/${provider}/redirect`,
             })
                 .then((res) => {
-                    newWindow.location.href = res.data;
+                    window.open(res.data, "", "width=1000,height=750");
+                    // newWindow.location.href = res.data;
                 })
                 .catch((err) => {
                     console.log(err.response);
                 });
         },
 
-        onMessage(e) {
-            if (e.origin !== window.origin || !e.data.access_token) {
-                return;
-            }
-            console.log(e);
-            userStore().$patch((state) => {
-                state.user = Object.assign({}, e.data.user, {
-                    access_token: res.data.access_token,
-                });
-                state.access_token = e.data.access_token;
-            });
+        // onMessage(e) {
+        //     console.log(e);
+        //     if (e.origin !== window.origin || !e.data.access_token) {
+        //         return;
+        //     }
+            
+        //     userStore().$patch((state) => {
+        //         state.user = Object.assign({}, e.data.user, {
+        //             access_token: res.data.access_token,
+        //         });
+        //         state.access_token = e.data.access_token;
+        //     });
 
-            this.$router.push("/dashboard");
-        },
+        //     this.$router.push("/dashboard");
+        // },
 
-        openWindow(url, title, options = {}) {
-            if (typeof url === "object") {
-                options = url;
-                url = "";
-            }
+        // openWindow(url, title, options = {}) {
+        //     if (typeof url === "object") {
+        //         options = url;
+        //         url = "";
+        //     }
 
-            options = { url, title, width: 600, height: 720, ...options };
+        //     options = { url, title, width: 600, height: 720, ...options };
 
-            const dualScreenLeft =
-                window.screenLeft !== undefined
-                    ? window.screenLeft
-                    : window.screen.left;
-            const dualScreenTop =
-                window.screenTop !== undefined
-                    ? window.screenTop
-                    : window.screen.top;
-            const width =
-                window.innerWidth ||
-                document.documentElement.clientWidth ||
-                window.screen.width;
-            const height =
-                window.innerHeight ||
-                document.documentElement.clientHeight ||
-                window.screen.height;
+        //     const dualScreenLeft =
+        //         window.screenLeft !== undefined
+        //             ? window.screenLeft
+        //             : window.screen.left;
+        //     const dualScreenTop =
+        //         window.screenTop !== undefined
+        //             ? window.screenTop
+        //             : window.screen.top;
+        //     const width =
+        //         window.innerWidth ||
+        //         document.documentElement.clientWidth ||
+        //         window.screen.width;
+        //     const height =
+        //         window.innerHeight ||
+        //         document.documentElement.clientHeight ||
+        //         window.screen.height;
 
-            options.left = width / 2 - options.width / 2 + dualScreenLeft;
-            options.top = height / 2 - options.height / 2 + dualScreenTop;
+        //     options.left = width / 2 - options.width / 2 + dualScreenLeft;
+        //     options.top = height / 2 - options.height / 2 + dualScreenTop;
 
-            const optionsStr = Object.keys(options)
-                .reduce((acc, key) => {
-                    acc.push(`${key}=${options[key]}`);
-                    return acc;
-                }, [])
-                .join(",");
+        //     const optionsStr = Object.keys(options)
+        //         .reduce((acc, key) => {
+        //             acc.push(`${key}=${options[key]}`);
+        //             return acc;
+        //         }, [])
+        //         .join(",");
 
-            const newWindow = window.open(url, title, optionsStr);
+        //     const newWindow = window.open(url, title, optionsStr);
 
-            if (window.focus) {
-                newWindow.focus();
-            }
+        //     if (window.focus) {
+        //         newWindow.focus();
+        //     }
 
-            return newWindow;
-        },
+        //     return newWindow;
+        // },
 
         authenticate() {
             this.$refs.login_btn.setAttribute("disabled", true);
@@ -219,12 +221,12 @@ export default {
     updated() {},
 
     beforeUnmount() {
-        window.removeEventListener("message", this.onMessage);
+        // window.removeEventListener("message", this.onMessage);
     },
 
     mounted() {
         // Waiting for the callback.blade.php message... (token and username).
-        window.addEventListener("message", this.onMessage, false);
+        // window.addEventListener("message", this.onMessage, false);
     },
 };
 </script>
