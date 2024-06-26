7e222aabf25385a633cffd3d8a449631c8f0fac5
ATG <j6cafe2018@gmail.com>
2024-06-26 23:17:08 +0800

    save

--- src/views/membership/MembershipStatus.vue
+++ src/views/membership/MembershipStatus.vue
@@ -155,6 +155,178 @@ onMounted(() => {
                         </div>
                     </div>
                 </div>
+
+                <div
+                    class="card w-100"
+                    v-if="subscription?.type.name == 'Regular'"
+                >
+                    <div class="card-body p-0 h-75">
+                        <div class="bg-info bg-gradient py-4 text-white">
+                            <h3 class="text-center">
+                                <i class="fa fa-gamepad h2"></i>
+                                Regular
+                            </h3>
+                            <div class="text-center">
+                                <h3>₱500 / month</h3>
+                            </div>
+                        </div>
+                        <div class="px-4 my-4 h6">
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Membership Loyalty Christmas Gift
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                VIP Units Access
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Computer Peripherals Access
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Game Request
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Technical Support
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Account No Limit
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Christmas Event
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Price Promo
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-times text-danger me-2"></i>
+                                Reservations Online
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-times text-danger me-2"></i>
+                                Tournament Reservation
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-times text-danger me-2"></i>
+                                Tournament Priority
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-times text-danger me-2"></i>
+                                Membership Bonus
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-times text-danger me-2"></i>
+                                Email / Call Support
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-times text-danger me-2"></i>
+                                SMS Notification
+                            </div>
+                        </div>
+                        <div class="m-4">
+                            <div
+                                class="btn btn-outline-primary bg-gradient w-100"
+                                @click="
+                                    stripe.state = true;
+                                    subscription_plan = 'regular';
+                                "
+                            >
+                                Subscribe
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div
+                    class="card w-100"
+                    v-if="subscription?.type.name == 'Loyal'"
+                >
+                    <div class="card-body p-0 h-75">
+                        <div class="bg-primary bg-gradient py-4 text-white">
+                            <h3 class="text-center">
+                                <i class="fa fa-gamepad h2"></i>
+                                Loyal
+                            </h3>
+                            <div class="text-center">
+                                <h3>₱750 / month</h3>
+                            </div>
+                        </div>
+                        <div class="px-4 my-4 h6">
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Membership Loyalty Christmas Gift
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                VIP Units Access
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Computer Peripherals Access
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Game Request
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Technical Support
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Account No Limit
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Christmas Event
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Price Promo
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Reservations Online
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Tournament Reservation
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Tournament Priority
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Membership Bonus
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                Email / Call Support
+                            </div>
+                            <div class="d-flex align-items-center py-1">
+                                <i class="fa fa-check text-success me-2"></i>
+                                SMS Notification
+                            </div>
+                        </div>
+                        <div class="m-4">
+                            <button
+                                class="btn btn-outline-primary bg-gradient w-100"
+                                @click="
+                                    stripe.state = true;
+                                    subscription_plan = 'loyal';
+                                "
+                            >
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="col-lg-4">
